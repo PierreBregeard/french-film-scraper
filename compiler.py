42514da8a6a7b623b2109818f8696756a6d3937e
Pierre <p.bregeard57@gmail.com>
2024-07-08 23:19:56 +0200

    add upx

--- compiler.py
+++ compiler.py
@@ -1,17 +1,10 @@
 from pathlib import Path
 import os
-from shutil import rmtree
 
 if __name__ == "__main__":
 
     root_path = Path(__file__).parent
     os.chdir(root_path)
-
-    # https://github.com/upx/upx/releases/tag/v4.2.3
-    # UPX_DIR_PATH = "UPX"
-    # os.system(
-    #     f"python3 -m PyInstaller --noconfirm --icon fav.ico --clean --console --onefile --upx-dir {UPX_DIR_PATH} --name {NAME} main.py"
-    # )
 
     is_windows = os.name == 'nt'
 
@@ -19,12 +12,9 @@ if __name__ == "__main__":
     if is_windows:
         name += '.exe'
 
-    command = f"python -m PyInstaller --noconfirm --icon fav.ico --clean --console --onefile --name {name} main.py"
+    # https://github.com/upx/upx/releases/tag/v4.2.4
+    command = f"python -m PyInstaller --noconfirm --icon fav.ico --clean --console --onefile --upx-dir upx --name {name} main.py"
     os.system(command)
 
     exe_folder = root_path / Path(f"dist/{name}")
     print(f"Built in {exe_folder}")
-
-    rmtree("build", ignore_errors=True)
-    if os.path.isfile("film-scraper.spec"):
-        os.remove("film-scraper.spec")
